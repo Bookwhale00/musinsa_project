1840031bc8e7846c65f3f4bdcdc5107edccc90c7
Bookwhale00 <Bookwhale00@gmail.com>
2023-04-07 11:39:44 +0900

    로그아웃 ok

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,19 @@
 #view 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect, HttpResponse
 from .forms import SignupForm
 from .forms import SigninForm
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+
 
 def home(request):
-    return redirect('/signin')
+    if request.user.is_authenticated:
+        return redirect('/product-create')
+    else:
+        return redirect('/signin')
 
 def signup(request):
     # 회원 가입 view
@@ -17,6 +23,10 @@ def signup(request):
             user = form.save()
             login(request, user)
             return redirect('/product-create')
+        else:
+            # 유효성 오류일 때 메시지
+            error_msg = '올바른 패스워드를 입력해주세요.'
+            return render(request, 'user/signup.html',{'form':form, 'error_msg':error_msg})
     else:
         form = SignupForm()
     return render(request, 'user/signup.html', {'form':form})
@@ -28,19 +38,23 @@ def signin(request):
         if form.is_valid():
             username = form.cleaned_data.get('username','')
             password = form.cleaned_data.get('password','')
-            user = authenticate(request, username=username, password=password)
+            print('username:', username)
+            print('password:', password) # 들어오는 데이터 출력 - 확인
+            user = auth.authenticate(request, username=username, password=password)
             if user is not None:
-                login(request, user)
+                auth.login(request, user)
                 return redirect('/product-create')
             
-            else:
+        else:
+            print('form.errors:', form.errors) # 검증 실패한 이유 좀 알자!
             # 인증 실패 시 오류 메시지
-                error_msg = '유저이름 혹은 패스워드를 확인해주세요.'
-                return render(request, 'user/signin.html',{'form':form, 'error_msg':error_msg})
-       
+            error_msg = '유저이름 혹은 패스워드를 확인해주세요.'
+            return render(request, 'user/signin.html',{'form':form, 'error_msg':error_msg})  
     else:
         form = SigninForm()
     return render(request, 'user/signin.html', {'form':form})
 
-# def user_logout(request):
+def user_logout(request):
 #     # 로그아웃 view
+    auth_logout(request)
+    return redirect('/')
